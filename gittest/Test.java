b29a02e97c11b58e30805465d82568e942f6aebd
Li <jiannanl@acbc32a258bf.ant.amazon.com>
2016-03-02 16:28:49 -0800

    changes in first_branch

--- gittest/Test.java
+++ gittest/Test.java
@@ -1,5 +1,8 @@
 public class Test{
     public static void main(String[] args) {
-
+        int[] array = new int[];
+        for(int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
     }
 }
