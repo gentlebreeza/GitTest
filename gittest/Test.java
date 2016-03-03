ff94ffffed03e4eeba72316e7bcc8eefbeee27b5
Li <jiannanl@acbc32a258bf.ant.amazon.com>
2016-03-03 13:23:39 -0800

    add a map

--- gittest/Test.java
+++ gittest/Test.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 public class Test{
     public static void main(String[] args) {
         int[] array = new int[];
         for(int i = 0; i < 10; i++) {
             array[i] = i;
         }
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "Casillas");
     }
-}
